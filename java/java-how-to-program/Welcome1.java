d121bf0ab50b03b774ae49043fa888b2ba6df571
Eli Pereira <eli.pereira@univille.br>
2018-06-22 20:29:03 -0300

    teste

---
+++ java/java-how-to-program/Welcome1.java
@@ -0,0 +1,8 @@
+// Figura 2.1 : Welcome1.java
+// Programa de impressão de texto
+public class Welcome1{
+    // Metodo main inicia a execução do aplicativo java
+    public static void main(String[] args){
+        System.out.println("Welcome to java Programming!");
+    }// fim do metodo main
+} // fim da classe Welcome1
